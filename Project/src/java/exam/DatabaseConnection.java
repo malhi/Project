9047cab20f408c0cc3ff4167102533a3d9d3ff55
c0653616 <c0653616@SB-N208-15.lambton.on.ca>
2015-08-04 19:02:36 -0400

    Database Connection file

---
+++ Project/src/java/exam/DatabaseConnection.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exam;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author c0653616
+ */
+public class DatabaseConnection {
+    public  Connection getConnection() {
+         Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            System.err.println("JDBC Driver Not Found: " + ex.getMessage());
+        }
+
+        try {
+            String jdbc = "jdbc:mysql://localhost/mydatabase";
+            conn = DriverManager.getConnection(jdbc, "root", "");
+        } catch (SQLException ex) {
+            System.err.println("Failed to Connect: " + ex.getMessage());
+        }
+        return conn;
+    }
+     
+     
+
+    
+    public int checkingDuplicateUserName(String username){
+        int rowCount=0;
+        try {
+            Connection con=getConnection();
+            String query="SELECT COUNT(*) count FROM users WHERE username=?";
+            PreparedStatement stmt=con.prepareStatement(query);
+            stmt.setString(1, username);
+            ResultSet rs=stmt.executeQuery();
+            while(rs.next()){
+                rowCount=rs.getInt("count");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return rowCount;
+    }
+    
+    public int checkingDuplicateEmailId(String email){
+        int rowCount=0;
+        try {
+            Connection con=getConnection();
+            String query="SELECT COUNT(*) count FROM users WHERE emailid=?";
+            PreparedStatement stmt=con.prepareStatement(query);
+            stmt.setString(1, email);
+            ResultSet rs=stmt.executeQuery();
+            while(rs.next()){
+                rowCount=rs.getInt("count");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return rowCount;
+    }
+}
